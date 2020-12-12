9608bbd4cf19f3fc46298e05f42393a24c5953c5
Joseph Bales <joseph.bales@gmail.com>
2020-12-12 01:28:52 -0600

    Add project files.

---
+++ ShutdownApp/MainForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace ShutdownApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(96, 139);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(287, 112);
+            this.CancelButton.TabIndex = 0;
+            this.CancelButton.Text = "Cancel Shutdown";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-878, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(62, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(354, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "System will shut down in 60 seconds.";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(485, 306);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CancelButton);
+            this.Name = "MainForm";
+            this.Text = "ShutdownApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ShutdownApp/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace ShutdownApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            ProcessStartInfo psinfo = new ProcessStartInfo();
+            psinfo.WindowStyle = ProcessWindowStyle.Hidden;
+            psinfo.FileName = "shutdown.exe";
+            psinfo.Arguments = "/s /t 60";
+
+            Process process = new Process();
+            process.StartInfo = psinfo;
+            process.Start();
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo psinfo = new ProcessStartInfo();
+            psinfo.WindowStyle = ProcessWindowStyle.Hidden;
+            psinfo.FileName = "shutdown.exe";
+            psinfo.Arguments = "/a";
+
+            Process process = new Process();
+            process.StartInfo = psinfo;
+            process.Start();
+            Environment.Exit(1);
+        }
+    }
+}
